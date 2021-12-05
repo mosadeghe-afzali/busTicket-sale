66b9b355ed57c433e46cb709924bf941d0f33e6d
mosadeghe <mosadegheafzali@yahoo.com>
2021-12-05 23:05:07 +0330

    create migration and model of payment and set relationships with Reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
 
 class Reservation extends Model
 {
+    use Notifiable;
     use HasFactory;
     use SoftDeletes;
 
@@ -26,4 +28,28 @@ class Reservation extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeCheckSchedule($query, $select)
+    {
+        $vehicle_callback = function ($query) use ($select) {
+            $query->select('model', 'id');
+        };
+
+        $schedule_callback = function ($query) use ($select, $vehicle_callback) {
+            $query->select($select)->with(['vehicle' => $vehicle_callback] );
+        };
+
+        return $query->with(['schedule' => $schedule_callback]);
+    }
+
+    public function scopeCheckPassenger($query, $select)
+    {
+        $passenger_callback = function ($query) use ($select) {
+            $query->select($select);
+        };
+
+        return $query->with(['passenger' => $passenger_callback]);
+    }
 }
+
+
